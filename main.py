9e2252c518dbe4a08bba90b2b3b2fdce861b0214
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-03-19 13:09:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+### Example Code 1
+
+fName = "Mr"
+lName = "Colley"
+
+print(fName)
+
+# Identify a variable in the code
+ # Answer:fName & lname 
+
+# Identify a string in the code
+ # Answer: "mr" & "Colley"
+
+# Line 4 is changed to lName = "Thorpe".  How does this affect the output?
+ #Answer: If it is change then the output will display as "Thorpe"and not "Colley"
+
+# Line 3 is changed to fName = "Mrs". How does this affect the output?
+ # Answer:If it is change then the output will display as "Mrs"and not "Mr"
+
+
+### Example Code 2
+
+num1 = 20
+num2 = 5
+
+total1 = num1 + 15
+total2 = num2 * 2
+total3 = num1 - num2
+
+print(total3)
+
+# What will be output by the program?
+ # Answer: The output of the program will be 15
+
+
+### Example Code 3
+
+name1 = "Ross" 
+name2 = "Monica" 
+name3 = "Joey" 
+name4 = "Rachel" 
+name5 = "Chandler" 
+ 
+print(name1 + " and " + name4) 
+print(name3) 
+ 
+name3 = "Phoebe" 
+
+
+# How many variables are used in the program?
+ # Answer: Theres 5 variables beinf use in this program
+
+# What would be the impact of changing print(name1,"and", name4)to print(name1,"and",name5)?
+ # Answer: Then name4 would chnage instantly to name 5 giving it anothe string
+
+# What is the purpose of the '+' symbol in  print(name1, " and ", name4) 
+ # Answer: to add the variable to an output
+
+# The line  print(name3) is added to the end of the code.  Explain what it will do.
+ # Answer: It was added at the end in a different line from "Ross and rachel" 
